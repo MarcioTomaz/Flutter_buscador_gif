f9087aba451a3767226d3777e2edc0460f5f5b63
marcioTomaz <marciotmaz@gmail.com>
2021-05-14 11:34:00 -0300

    iniciando layout

--- lib/main.dart
+++ lib/main.dart
@@ -6,5 +6,16 @@ import 'package:flutter_buscador_gifs/ui/home_page.dart';
 void main(){
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+        )
+    ),
   ));
 }
